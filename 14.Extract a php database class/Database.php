f597c79ac8953ff41e0a50a45f3b61077b508731
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-24 13:31:51 +0530

    updated

--- 14.Extract a php database class/Database.php
+++ 14.Extract a php database class/Database.php
@@ -6,7 +6,7 @@ class Database
 
     public function __construct()
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $dsn = "mysql:host=localhost;port=3306;dbname=phpbeginner;user=root;charset=utf8mb4";
 
         $this->connection = new PDO($dsn);
     }
