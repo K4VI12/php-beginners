f6afe0d8913a617cbeddcc4bbd2cb13cb1c58f6e
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-24 12:58:41 +0530

    connected the mysql database

--- 13.Create mysql database/index.php
+++ 13.Create mysql database/index.php
@@ -1,4 +1,16 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+
+$dsn = "mysql:host=<localhost></localhost:3300>;port=3306;dbname=phpbeginner;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from post");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
