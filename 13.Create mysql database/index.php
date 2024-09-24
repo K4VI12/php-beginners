a61e26c7df169e50e0e034fb703410759a1f6cc5
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-24 13:08:43 +0530

    updated

--- 13.Create mysql database/index.php
+++ 13.Create mysql database/index.php
@@ -2,11 +2,10 @@
 
 require 'functions.php';
 
-$dsn = "mysql:host=<localhost></localhost:3300>;port=3306;dbname=phpbeginner;user=root;charset=utf8mb4";
+$dsn = "mysql:host=localhost;port=3306;dbname=phpbeginner;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root', ''); // Add password if necessary
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from post");
+$statement = $pdo->prepare("SELECT * FROM post");
 $statement->execute();
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
