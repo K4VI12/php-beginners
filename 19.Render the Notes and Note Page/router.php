a5d790479814f3eeec424ef93d730154361afb3d
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 00:55:16 +0530

    error

--- 19.Render the Notes and Note Page/router.php
+++ 19.Render the Notes and Note Page/router.php
@@ -5,6 +5,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
     '/contact' => 'controllers/contact.php',
 ];
 
