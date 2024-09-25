40fb7ea787814fe57958b48e4b2a50661b3bd98d
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 22:54:51 +0530

    have error in loding after moving to core

--- 27.Autoloading and Extraction/public/index.php
+++ 27.Autoloading and Extraction/public/index.php
@@ -7,7 +7,7 @@ require BASE_PATH . 'functions.php';
 
 
 spl_autoload_register(function ($class) {
-    require base_path($class . '.php');
+    require base_path("Core/{$class}.php");
 });
 
 require  base_path('router.php');
