f0c66a6a50062b1f7f8b6eca6b558dc80703279e
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 11:21:53 +0530

    update

--- 20.Introduction to Authorization/controllers/note.php
+++ 20.Introduction to Authorization/controllers/note.php
@@ -5,11 +5,25 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 
-$heading = 'My Note';
+$heading = 'Note';
 
 $id = $_GET['id'];
 
-$note = $db-> query('SELECT * FROM notes where id =  :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes where id = :id',[
+        
+        'id' => $_GET['id']
+
+])->fetch();
+
+if(! $note){
+    abort();
+}
+
+
+if($note['user_id'] != 1){ //1 means in signify the current user
+    abort(403);
+}
+
 
 
 require "views/note.view.php";
