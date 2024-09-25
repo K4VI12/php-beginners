1cf775a781e42dada29e171e642b5114110b74df
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 21:03:06 +0530

    fix the database issu
    in the Notes tab have create note form , its have save button for save the body (message) its fixed

--- 27.Autoloading and Extraction/controllers/notes/create.php
+++ 27.Autoloading and Extraction/controllers/notes/create.php
@@ -1,18 +1,20 @@
 <?php
 
-require 'Validator.php';
+require base_path('Validator.php');
 
 
 $config = require base_path('config.php');
 
 $db = new Database($config['database']);
 
+$errors = [];
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
 
 
-    if (! $validator->string($_POST['body'], 1, 1000)) {
+
+    if (! Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = 'A body of more than 1,000 characters is required';
     }
 
@@ -28,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 view("notes/create.view.php", [
-    'heading'=> 'Create Note',
-    'errors'=> '$errors'
+    'heading' => 'Create Note',
+    'errors' => '$errors'
 
 ]);
