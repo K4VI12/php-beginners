a095bb6863c584b541893ec6bb6fc9afa2caf582
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 21:13:59 +0530

    fixed the issu

--- 27.Autoloading and Extraction/public/index.php
+++ 27.Autoloading and Extraction/public/index.php
@@ -1,10 +1,14 @@
 <?php
 
-const BASE_PATH = __DIR__ . '/../';
+const BASE_PATH = __DIR__. '/../'; //declare constant
 
 
-require BASE_PATH . 'functions.php';
+require BASE_PATH. 'functions.php';
 
-require  base_path('Database.php');
-require  base_path('Response.php');
+
+spl_autoload_register(function ($class) 
+{
+    require base_path($class . '.php');
+});
+
 require  base_path('router.php');
