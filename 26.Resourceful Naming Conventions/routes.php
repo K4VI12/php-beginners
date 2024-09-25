8ca2122506844295863ab7861d77a646d3e06805
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 20:10:34 +0530

    fixed

--- 26.Resourceful Naming Conventions/routes.php
+++ 26.Resourceful Naming Conventions/routes.php
@@ -4,7 +4,7 @@ return [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/note/show.php',
+    '/note' => 'controllers/notes/show.php',
     '/notes/create' => 'controllers/notes/create.php',
     '/contact' => 'controllers/contact.php',
 ];
