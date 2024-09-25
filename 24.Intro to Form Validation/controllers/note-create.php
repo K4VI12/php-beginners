f454acb3b4a4aef2fe6592f7dfd39b18f49080ef
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-25 15:47:55 +0530

    updating

--- 24.Intro to Form Validation/controllers/note-create.php
+++ 24.Intro to Form Validation/controllers/note-create.php
@@ -7,10 +7,22 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body , :user_id)', [
-        'body' => $_POST['body'],
-        'user_id'=>1
-    ]);
+
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+
+        $errors['body'] = 'A body is required';
+    }
+
+    if(empty($errors)){
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body , :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+
+
 }
 
 require 'views/note-create.view.php';
--- 24.Intro to Form Validation/views/note-create.view.php
+++ 24.Intro to Form Validation/views/note-create.view.php
@@ -13,16 +13,20 @@
                                 <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
 
                                 <div class="mt-1">
-                                    <textarea id="body" name="body" rows="3"
-                                              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                              placeholder="Here's an idea for a note..."></textarea>
+                                    <textarea id="body"
+                                        name="body"
+                                        rows="3"
+                                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Here's an idea for a note..."
+                                        required
+                                        ></textarea>
                                 </div>
                             </div>
                         </div>
 
                         <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
                             <button type="submit"
-                                    class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                                 Save
                             </button>
                         </div>
