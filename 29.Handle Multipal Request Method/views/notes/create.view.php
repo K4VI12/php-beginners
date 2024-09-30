f7d52b5df4671b89eaee54f59c04f6fd1d51e89a
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-30 14:13:48 +0530

    Handle Multipal Request Method

--- 29.Handle Multipal Request Method/views/notes/create.view.php
+++ 29.Handle Multipal Request Method/views/notes/create.view.php
@@ -22,8 +22,7 @@
                                         rows="3"
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                         placeholder="Here's an idea for a note..."
-                                    ><?= $_POST['body'] ?? '' ?>
-                                </textarea>
+                                    ><?= $_POST['body'] ?? '' ?></textarea>
 
                                     <?php if (isset($errors['body'])) : ?>
                                         <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
