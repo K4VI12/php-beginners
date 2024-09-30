0838ed87a68f677412d860951b7f12a6a19d116e
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-09-30 15:17:08 +0530

    fiexd

--- 31/Core/Database.php
+++ 31/Core/Database.php
@@ -9,7 +9,7 @@ class Database
     public $connection;
     public $statement;
 
-    public function __construct($config, $username = 'root', $password = '')
+    public function __construct($config, $username = 'root', $password = '1234')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
